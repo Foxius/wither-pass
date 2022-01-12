70ad0c48cc526bd229ff3a09d6b947a016eea26d
Foxius <81900523+Foxius@users.noreply.github.com>
2022-01-12 11:00:56 +0500

    Update QuestRegistry.java

--- src/main/java/io/github/battlepass/registry/quest/QuestRegistry.java
+++ src/main/java/io/github/battlepass/registry/quest/QuestRegistry.java
@@ -1,6 +1,5 @@
 package io.github.battlepass.registry.quest;
 
-import io.github.battlepass.quests.service.base.ExternalQuestContainer;
 import io.github.battlepass.quests.service.base.QuestContainer;
 import io.github.battlepass.registry.quest.object.PluginVersion;
 import me.hyfe.simplespigot.annotations.NotNull;
@@ -44,36 +43,4 @@ public interface QuestRegistry extends Registry {
      */
     void quest(Instantiator<QuestContainer>... instantiators);
 
-    /**
-     * Attempts to hook into a plugin and will automatically retry if the plugin is not found (might not yet be loaded).
-     * This should be used to register external plugin hooks.
-     *
-     * @param name         The name of the plugin
-     * @param instantiator The instantiator to initialize the quest if appropriate
-     * @param author       The author to filter by. If empty it will not filter by the author
-     * @return Whether the hook was successful. Bear in mind it may not be successful initially but BattlePass will continue to try register it.
-     */
-    boolean hook(String name, Instantiator<ExternalQuestContainer> instantiator, String author);
-
-    /**
-     * Same as {@link QuestRegistry#hook(String, Instantiator, String)} without the author filter.
-     */
-    default void hook(String name, Instantiator<ExternalQuestContainer> instantiator) {
-        this.hook(name, instantiator, "");
-    }
-
-    /**
-     * Same as {@link QuestRegistry#hook(String, Instantiator, String)} but it also checks the version of the plugin.
-     *
-     * @param versionPredicate A tester to check if the version is x. Whatever you want with it.
-     */
-    boolean hook(String name, Instantiator<ExternalQuestContainer> instantiator, String author, Predicate<PluginVersion> versionPredicate);
-
-
-    /**
-     * Same as {@link QuestRegistry#hook(String, Instantiator, String, Predicate)} but without the author check.
-     */
-    default void hook(String name, Instantiator<ExternalQuestContainer> instantiator, Predicate<PluginVersion> versionPredicate) {
-        this.hook(name, instantiator, "", versionPredicate);
-    }
 }
